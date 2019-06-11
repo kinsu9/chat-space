8e03d0c12de13b70d453ab9cee8b0d53258f0470
hiroki <bis.at.kinsu@gmail.com>
2019-06-11 12:25:11 +0900

    ruby.vertion_update

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rbenv_type, :user
 
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, '2.5.1p57'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/kinsuec2.pem']
